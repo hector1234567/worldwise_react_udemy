3c85c7d397b2364e8e9318ac534af1ddcb81fc3f
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2023-12-07 19:29:18 +0100

    Starter code y páginas del proyecto

--- worldwise_react_udemy/src/App.jsx
+++ worldwise_react_udemy/src/App.jsx
@@ -1,19 +1,21 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import './App.css'
 import Product from './pages/Product'
 import Homepage from './pages/Homepage'
 import Pricing from './pages/Pricing'
 import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
+import AppLayout from './pages/AppLayout'
 
 function App() {
   return (
     <>
-      <h1>React Router!</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage/>}></Route>
           <Route path="product" element={<Product/>}></Route>
           <Route path="pricing" element={<Pricing/>}></Route>
+          <Route path="login" element={<Login/>}></Route>
+          <Route path="app" element={<AppLayout/>}></Route>
           <Route path="*" element={<PageNotFound/>}></Route>
         </Routes>
       </BrowserRouter>
--- worldwise_react_udemy/src/pages/PageNotFound.jsx
+++ worldwise_react_udemy/src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-import PageNav from "../components/PageNav";
-
-function PageNotFound() {
-    return (
-        <>
-            <PageNav/>
-            <h1>PageNotFound</h1>
-        </>
-    )
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
-
-export default PageNotFound;

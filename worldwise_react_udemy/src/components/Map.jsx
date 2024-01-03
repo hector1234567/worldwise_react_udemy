32e9e00dac9113f7f0299fe28d74a2955f50504a
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2024-01-03 13:40:00 +0100

    Popups con marcas en el mapa

--- worldwise_react_udemy/src/components/Map.jsx
+++ worldwise_react_udemy/src/components/Map.jsx
@@ -1,15 +1,19 @@
-import { useNavigate, useSearchParams } from "react-router-dom"
+// import { useNavigate, useSearchParams } from "react-router-dom"
 import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 import styles from "./Map.module.css"
 import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
     const [mapPosition, setMapPosition] = useState([40, 0])
-    const [searchParams] = useSearchParams();
-    const navigate = useNavigate();
+    // const [searchParams] = useSearchParams();
+    // const navigate = useNavigate();
 
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
+    // const lat = searchParams.get('lat');
+    // const lng = searchParams.get('lng');
+
+    const {cities} = useCities();
+    console.log(cities)
     return (
         <div className={styles.mapContainer}>
             <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} className={styles.map}>
@@ -17,11 +21,13 @@ function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {cities.map((city) => (
+                    <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+                        <Popup>
+                        <span>{city.emoji}</span> <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     )

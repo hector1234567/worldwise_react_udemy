0002ae516e1c93090e8c3a5198dbaf0617e22129
hector1234567 <52801178+hector1234567@users.noreply.github.com>
2024-01-03 12:57:08 +0100

    Mapa con Leaflet
    npm i react-leaflet leaflet

--- worldwise_react_udemy/src/components/Map.jsx
+++ worldwise_react_udemy/src/components/Map.jsx
@@ -1,14 +1,28 @@
 import { useNavigate, useSearchParams } from "react-router-dom"
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 import styles from "./Map.module.css"
+import { useState } from "react";
 
 function Map() {
+    const [mapPosition, setMapPosition] = useState([40, 0])
     const [searchParams] = useSearchParams();
     const navigate = useNavigate();
+
+    const lat = searchParams.get('lat');
+    const lng = searchParams.get('lng');
     return (
-        <div className={styles.mapContainer} onClick={() => navigate('form')}>
-            Map
-            <h1>Lat: {searchParams.get('lat')}</h1>
-            <h1>Lng: {searchParams.get('lng')}</h1>
+        <div className={styles.mapContainer}>
+            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} className={styles.map}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                />
+                <Marker position={mapPosition}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
